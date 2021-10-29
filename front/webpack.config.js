fa2e6df9951e8a0e96813b70826bcd914f24f1a0
Shuvani <gnomik2311@mail.ru>
2021-10-29 10:17:34 +0300

    Create react skeleton

---
+++ front/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    // tell where webpack will put created files
+    entry: './app/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    //define the type of transformations that gonna be maked on our code
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                },
+            },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'app', 'index.html'),
+        })
+    ]
+}
