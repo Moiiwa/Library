60eb55bc5b8613987341372b4b67e239502610f9
mihailgudkov <moiiwa@icloud.com>
2021-11-08 22:42:12 +0300

    Added book adding feature

---
+++ Library-Backend/src/main/java/Library/services/BookService.java
@@ -0,0 +1,17 @@
+package Library.services;
+
+import Library.entities.Book;
+import Library.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    @Autowired
+    BookRepository repository;
+
+    public void addBook(Book book){
+        repository.save(book);
+    }
+}
