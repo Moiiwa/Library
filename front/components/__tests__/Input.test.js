949b941ff1fbfddc30464c663313e1fa8cb49b2b
Shuvani <gnomik2311@mail.ru>
2021-11-11 03:58:41 +0300

    tests for all components

--- front/components/__tests__/Input.test.js
+++ front/components/__tests__/Input.test.js
@@ -3,6 +3,7 @@ import Input from '../Input';
 import toJson from 'enzyme-to-json';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { jest } from '@jest/globals';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -25,16 +26,14 @@ describe('Input', () => {
     });
 
     it('formatMessage state is true when right isbn', () => {
-        const wrapper = shallow(<Input />);
-        wrapper.find('input').simulate('change', { target: { value: '666-6-6666-6666-6' } });
-        wrapper.find('button').simulate('click')
-        expect(wrapper.instance().state.formatMessage).toBe(false);
-    });
 
-    it('formatMessage state is true when wrong isbn', () => {
-        const wrapper = shallow(<Input />);
-        wrapper.find('input').simulate('change', { target: { value: '666-6' } });
-        wrapper.find('button').simulate('click')
-        expect(wrapper.instance().state.formatMessage).toBe(true);
+        const wrapper = shallow(<Input />)
+        const instance = wrapper.instance()
+        const handleSubmitMock = jest.spyOn(instance, 'handleSubmit')
+        instance.forceUpdate();
+
+        const but = wrapper.find('button')
+        but.simulate('click')
+        expect(handleSubmitMock).toHaveBeenCalled()
     });
 });
