342d56724716c3a182ac5346ed9d3d802d525d06
mihailgudkov <moiiwa@icloud.com>
2021-11-16 22:41:41 +0300

    Added tests

--- Library-Backend/src/main/java/Library/controllers/BookController.java
+++ Library-Backend/src/main/java/Library/controllers/BookController.java
@@ -1,18 +1,15 @@
 package Library.controllers;
 
 import Library.dto.AddBookDto;
-import Library.dto.GetBookDto;
 import Library.dto.SellRentBookDto;
 import Library.entities.Book;
 import Library.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @CrossOrigin
 @RestController
